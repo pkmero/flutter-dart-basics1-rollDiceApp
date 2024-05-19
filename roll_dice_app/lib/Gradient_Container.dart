ffbe9ebd64a039d819cbbf4751fcf0991bbd8893
Amr-Pk <darkkillramrgamal@gmail.com>
2024-05-19 17:43:27 +0300

    creating gradientContainer

---
+++ roll_dice_app/lib/Gradient_Container.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// how to make a calss ?
+// start with uppercase like camlecase GradientContainer and be descrebtive to what it does
+//extends is a dart keyword used to inherit from stateless widget like data and logic that is required by flutter
+// when flutter detect GradientContainer in other widgets it will summon below widgets
+// in build method flutter detects and calls the function when it renders ui
+class GradientContainer extends StatelessWidget {
+  // GradientContainer has blue line as it need named key parameter
+  // positional arguments can be added in (a, b) named argument or paramaterss go in {a, b} but here we need a special word key
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 0, 0, 0),
+            Color.fromARGB(255, 255, 255, 255)
+          ],
+          begin: Alignment
+              .topLeft, // Alignment. is a trick instead of using x and y and i can always search docs
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Roll The Dice',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
+import 'Gradient_Container.dart';
 // void is main entry point
 // material app is root widget
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         // cannot take text like material app it takes body
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 0, 0, 0),
-                Color.fromARGB(255, 255, 255, 255)
-              ],
-              begin: Alignment
-                  .topLeft, // Alignment. is a trick instead of using x and y and i can always search docs
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Roll The Dice',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
